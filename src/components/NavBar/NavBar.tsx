508c947add14e1b788ab66dd68c7298c157a736e
Gharsa Amin <ghars0718@gmail.com>
2025-01-27 15:43:05 -0500

    added the nav-bar component

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+
+export default function navBar ()  {
+
+
+
+
+    return(
+        <section className="nav">
+            <p>
+                <a href="#home">Home</a>
+            </p>
+            <p>
+                <a href = "#blog">Blog</a>
+            </p>
+            <p>
+                <a href = "#projects">Projects</a>
+            </p>
+
+
+        </section>
+    )
+}
