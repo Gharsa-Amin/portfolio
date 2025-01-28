89199d36bc6e78d8713091bf63266e0677d48c0e
Gharsa Amin <ghars0718@gmail.com>
2025-01-27 23:04:10 -0500

    added svgs for each skill

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import "./App.css";
 import FooterSection from "./components/Footer/Footer";
+import Skills from "./components/Skills/Skills";
 
 	function App() {
 	return (
 		<BrowserRouter>
 		<NavBar />
 		<Routes>
-			<Route path="/" element={<div>Home Page</div>} />
+			<Route path="/" element={<Skills />} />
 			<Route path="/blog" element={<div>Blog Page</div>} />
 			<Route path="/projects" element={<div>Projects Page</div>} />
 		</Routes>
