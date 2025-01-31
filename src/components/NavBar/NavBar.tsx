7e393330a9e5211296abc8a525da4ff999ca43c6
Gharsa Amin <ghars0718@gmail.com>
2025-01-30 23:22:49 -0500

    added theme colors to the background

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom"; // You can still use Link here for consistent styling
-
+import "./NavBar.scss"; 
 const NavBar: React.FC = () => {
   const [theme, setTheme] = useState<string>(() => {
     return localStorage.getItem("theme") || "light"
@@ -23,6 +23,9 @@ const NavBar: React.FC = () => {
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light" ))
   }
+
+  const lightThemeGif =  "https://media.giphy.com/media/abBeARbqBhp8Qyr1k3/giphy.gif?cid=790b761142doo36grodftyzlonx61fcmycr40iy2assf1di6&ep=v1_gifs_search&rid=giphy.gif&ct=g"; 
+  const darkThemeGif = "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExYzgya2duYWV6MDl6YjV3N3E4bm5tdmxlaGdqcGJoMDQyMHp1bDd2MiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/pzC2KawpHBsli/giphy.gif"
   return (
     <section className="nav">
       <p>
@@ -37,9 +40,13 @@ const NavBar: React.FC = () => {
       <p>
         <Link to="/#blog">Blog</Link>
       </p>
-      <button>
-        Toggle Color {/* You can still add color toggle functionality */}
-      </button>
+      <div className="theme-gif">
+        <img
+          src={theme === "light" ? lightThemeGif : darkThemeGif}
+          alt={theme === "light" ? "Light theme GIF" : "Dark theme GIF"}
+          onClick={toggleTheme}
+        />
+      </div>
     </section>
   );
 };
