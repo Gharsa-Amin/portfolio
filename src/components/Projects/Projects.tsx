e180aeff589694efbae1a5cae5da2bdb3a271230
Gharsa Amin <ghars0718@gmail.com>
2025-02-03 13:30:51 -0500

    added responsive designs for each project

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,7 +1,9 @@
 import "./Projects.scss"; 
 import React from "react";
 import { Link } from "react-router-dom";
-import crypto from "../../assets/images/Crypto-Trading.png"
+import crypto from "../../assets/images/CryptoTrading.png"
+import InStock from "../../assets/images/InStock.png"; 
+import Snaps from "../../assets/images/Snaps.png "; 
 
 const Projects: React.FC = () => {
     return (
@@ -18,8 +20,6 @@ const Projects: React.FC = () => {
                 <a href="https://www.loom.com/share/1bc8eaf48c16401ab8231fd55a9dafd3?sid=b2fd3e03-f882-440a-8d9d-b955ffc2ed33" target="_blank" rel="noopener noreferrer">
                     <button>Live Demo</button>
                 </a>
-           
-                
                 <div>
                     <svg
                         width="20px"
@@ -49,6 +49,8 @@ const Projects: React.FC = () => {
             </section>
             <section className="card"> 
                 <h2>SNAPS</h2>
+                
+
                 <p>A full-stack, mobile-first crypto-trading platform with real-time trading functionalities and a blockchain featuring a decentralized network where transactions are securely recorded and are using proof-of-existence.</p>
                 <p>Tech Stack: React, Express.js, Node.js, MySQL, Chart.js, OAuth, JWT, External APIs</p>
                 <a href="https://www.loom.com/share/1bc8eaf48c16401ab8231fd55a9dafd3?sid=b2fd3e03-f882-440a-8d9d-b955ffc2ed33" target="_blank" rel="noopener noreferrer">
@@ -85,6 +87,7 @@ const Projects: React.FC = () => {
 
 <section className="card"> 
 <h2>In Stock</h2>
+<img src={InStock} width="50px" alt="" />
 <p>A full-stack, mobile-first crypto-trading platform with real-time trading functionalities and a blockchain featuring a decentralized network where transactions are securely recorded and are using proof-of-existence.</p>
 <p>Tech Stack: React, Express.js, Node.js, MySQL, Chart.js, OAuth, JWT, External APIs</p>
 <a href="https://www.loom.com/share/1bc8eaf48c16401ab8231fd55a9dafd3?sid=b2fd3e03-f882-440a-8d9d-b955ffc2ed33" target="_blank" rel="noopener noreferrer">
