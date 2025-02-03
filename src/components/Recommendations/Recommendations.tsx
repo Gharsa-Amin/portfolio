e1e344091088d92b9a379ff9e70004831ab6bc82
Gharsa Amin <ghars0718@gmail.com>
2025-02-02 21:49:49 -0500

    added animations to the recommendation cards

--- src/components/Recommendations/Recommendations.tsx
+++ src/components/Recommendations/Recommendations.tsx
@@ -15,6 +15,7 @@ const Recommendations: React.FC = () => {
             <div>
                 Recommendations and Collaborations 
             </div>
+            <section className="recommendations-wrapper">
             <section className="recommendations-card">
                 <div className="recommendations-header">
                 <img className="recommendations-headshot" src={Venessa} alt="" />
@@ -23,7 +24,7 @@ const Recommendations: React.FC = () => {
                     </p>
                 
                 <p>I had the pleasure of being classmates with Gharsa at BrainStation, where she stood out as a proactive learner, always eager to answer questions in class and volunteer for tasks. I also had the opportunity to collaborate with her on a stock inventory project and she was an absolute joy to work with. Gharsa consistently ensured her tasks were completed on time and demonstrated exceptional communication skills throughout the project.
-                    I highly recommend Gharsa to join any team as she would be a valuable member to any workplace.</p>
+                    I highly recommend Gharsa to join any team as she would be a valuable member.</p>
                     <a href="https://www.linkedin.com/in/gharsanay-amin/details/recommendations/?detailScreenTabIndex=0" >
                     <button>Visit the page <ArrowUp /></button> </a>
             </section>
@@ -52,7 +53,7 @@ She is a natural leader who always steps up to the plate, is not afraid to ask t
                 
                 <p>I’m excited to recommend Gharsa, a talented and dedicated software engineer who has recently graduated. Skilled in Node.js, React.js, and building scalable web applications, Gharsa consistently demonstrated exceptional problem-solving and collaboration abilities during our time working together on different projects during the program. 
 
-Gharsa’s strong technical foundation, innovative mindset, and eagerness to learn make them an excellent fit for any team. I have no doubt they’ll excel in their career and contribute meaningfully to any organization! </p>
+Gharsa’s strong technical foundation, innovative mindset, and eagerness to learn make them an excellent fit for any team. She will excel in her career! </p>
                 <a href="https://www.linkedin.com/in/gharsanay-amin/details/recommendations/?detailScreenTabIndex=0" >
                 <button>Visit the page 
                 <ArrowUp /></button> </a>
@@ -64,11 +65,10 @@ Gharsa’s strong technical foundation, innovative mindset, and eagerness to lea
                 <p>Ayesha Banu</p></div>
                 <p>Brainstation Software Engineering <GraduateSVG /></p>
                
-            <p>I had the pleasure of working alongside Gharsa during our time at BrainStation, and I can confidently say that she is an exceptional individual. She is not only highly knowledgeable in Web Development, but she also has a remarkable ability to communicate complex ideas in an approachable way.
+            <p>I can confidently say that Gharsa is an exceptional individual. She is not only highly knowledgeable in Web Development, but she also has a remarkable ability to communicate complex ideas in an approachable way.
 
-What sets Gharsa apart is her positive attitude and willingness to help others. She is always open to collaboration and provides valuable insights that help elevate the work of those around her. Her work ethic is impeccable, and she consistently delivers high-quality results while maintaining a friendly and supportive demeanor.
-
-Whether working on group projects or having casual discussions, it is clear that Gharsa is someone who brings both intelligence and kindness to any environment. I look forward to seeing her future accomplishments and can’t recommend her enough for any opportunity she pursues.</p>
+What sets Gharsa apart is her positive attitude and willingness to help others, provides valuable insights that help elevate the work of those around her. Her work ethic is impeccable, and she consistently delivers high-quality results while maintaining a friendly and supportive demeanor.
+</p>
 <a href="https://www.linkedin.com/in/gharsanay-amin/details/recommendations/?detailScreenTabIndex=0" >
                 <button>Visit the page 
                     <ArrowUp />
@@ -76,11 +76,68 @@ Whether working on group projects or having casual discussions, it is clear that
                     
                      </a>
             </section>
+            <section className="recommendations-card">
+                <div className="recommendations-header">
+                <img className="recommendations-headshot" src={Venessa} alt="" />
+                <p>Vanessa Tseng</p></div>
+                <p>Brainstation Software Engineering <GraduateSVG />
+                    </p>
+                
+                <p>I had the pleasure of being classmates with Gharsa at BrainStation, where she stood out as a proactive learner, always eager to answer questions in class and volunteer for tasks. I also had the opportunity to collaborate with her on a stock inventory project and she was an absolute joy to work with. Gharsa consistently ensured her tasks were completed on time and demonstrated exceptional communication skills throughout the project.
+                    I highly recommend Gharsa to join any team as she would be a valuable member.</p>
+                    <a href="https://www.linkedin.com/in/gharsanay-amin/details/recommendations/?detailScreenTabIndex=0" >
+                    <button>Visit the page <ArrowUp /></button> </a>
+            </section>
+            <section className="recommendations-card">
+            <div className="recommendations-header">
+               
+                <img className="recommendations-headshot" src={Laura} alt="" /> 
+                <p>Laura Courtney</p></div>
+                <p>Brainstation Software Engineering <GraduateSVG /></p>
+              
+                <p>Gharsa is such an absolute delight to work and learn along side. 
 
+Not only is she extremely intelligent and hard working, but she is also kind and always takes the time to ensure full and clear communication. She is uplifting, motivational and works extremely well with other people. 
 
+She is a natural leader who always steps up to the plate, is not afraid to ask the important questions, and who supports and inspires everyone around her. A true asset to any team and an all around wonderful person.</p>
+<a href="https://www.linkedin.com/in/gharsanay-amin/details/recommendations/?detailScreenTabIndex=0" >
+<button>Visit the page <ArrowUp /></button> </a>
+            </section>
 
+            <section className="recommendations-card">
+            <div className="recommendations-header">
+               
+                <img className="recommendations-headshot" src={Sivi} alt="" /> 
+                <p>Sivi Rakaj</p></div>
+                <p>Brainstation Software Engineering <GraduateSVG /></p>
+                
+                <p>I’m excited to recommend Gharsa, a talented and dedicated software engineer who has recently graduated. Skilled in Node.js, React.js, and building scalable web applications, Gharsa consistently demonstrated exceptional problem-solving and collaboration abilities during our time working together on different projects during the program. 
 
+Gharsa’s strong technical foundation, innovative mindset, and eagerness to learn make them an excellent fit for any team. She will excel in her career! </p>
+                <a href="https://www.linkedin.com/in/gharsanay-amin/details/recommendations/?detailScreenTabIndex=0" >
+                <button>Visit the page 
+                <ArrowUp /></button> </a>
+            </section>
+            <section className="recommendations-card">
+            <div className="recommendations-header">
+               
+                <img className="recommendations-headshot" src={Ayesha} alt="" /> 
+                <p>Ayesha Banu</p></div>
+                <p>Brainstation Software Engineering <GraduateSVG /></p>
+               
+            <p>I can confidently say that Gharsa is an exceptional individual. She is not only highly knowledgeable in Web Development, but she also has a remarkable ability to communicate complex ideas in an approachable way.
 
+What sets Gharsa apart is her positive attitude and willingness to help others, provides valuable insights that help elevate the work of those around her. Her work ethic is impeccable, and she consistently delivers high-quality results while maintaining a friendly and supportive demeanor.
+</p>
+<a href="https://www.linkedin.com/in/gharsanay-amin/details/recommendations/?detailScreenTabIndex=0" >
+                <button>Visit the page 
+                    <ArrowUp />
+                    </button>
+                    
+                     </a>
+            </section>
+            </section>
+            
         </section>
     );
 };
