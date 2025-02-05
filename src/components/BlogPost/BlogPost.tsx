e3849b2b1eb867f646014bb73059c892d2406984
Gharsa Amin <ghars0718@gmail.com>
2025-02-04 21:11:58 -0500

    added Link and arrows to move from each blog post to the main blogpage

--- src/components/BlogPost/BlogPost.tsx
+++ src/components/BlogPost/BlogPost.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import blogPosts from "../../assets/data/Blog.json"; 
+import { Link } from "react-router-dom";
 import "./BlogPost.scss"; 
 const BlogPost: React.FC = () => {
   const { postId } = useParams<{ postId: string }>(); 
@@ -29,8 +30,24 @@ const BlogPost: React.FC = () => {
   }
 
   return (
-    <div className="blog-post-detail">
-      <h1 className="blog-post-header">{post.title}</h1>
+    <div className="blog-post-detail"> 
+    <Link to="/blog" >
+    <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    width="40px"
+    strokeWidth="1.5"
+    stroke="currentColor"
+    className="size-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18"
+    />
+  </svg>
+      <h1 className="blog-post-header">{post.title}</h1></Link>
       <p className="blog-post-paragraph"><strong>Author:</strong> {post.author}</p>
       <p className="blog-post-paragraph"><strong>Tags:</strong> {post.tags.join(", ")}</p>
       <p className="blog-post-paragraph"><strong>Date:</strong> {post.date}</p>
--- src/components/PopUp/PopUp.tsx
+++ src/components/PopUp/PopUp.tsx
@@ -9,19 +9,19 @@ interface WelcomePopUpProps {
 const WelcomePopUp: React.FC<WelcomePopUpProps> = ({ imageUrl }) => {
   const [visible, setVisible] = useState<boolean>(true);
 
-//   useEffect(() => {
-//     const timer = setTimeout(() => {
-//       setVisible(false); 
-//     }, 5000);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(false); 
+    }, 10000);
 
-//     return () => clearTimeout(timer); 
-//   }, []);
+    return () => clearTimeout(timer); 
+  }, []);
 
   return (
     <div className={`popup-container ${visible ? 'show' : 'hide'}`}>
       <div className="popup-content">
         <img src={imageUrl} alt="Welcome" className="popup-image" />
-        <p className="initial-popUp">I am Gharsa, Welcome!</p>
+        <p className="initial-popUp">Welcome!</p>
       </div>
     </div>
   );
